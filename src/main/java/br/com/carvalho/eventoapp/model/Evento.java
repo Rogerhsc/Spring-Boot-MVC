c7552a13d3ac09703501d38008440bb4e3ba1fee
euller.carvalho <e_h_carvalho@hotmail.com>
2018-04-14 13:17:33 -0300

    Adicionado dependencias, anotacoes e classes para acesso, criacao e insercao das/nas tabelas no banco de dados

---
+++ src/main/java/br/com/carvalho/eventoapp/model/Evento.java
@@ -0,0 +1,56 @@
+package br.com.carvalho.eventoapp.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Evento implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	private long codigo;
+	
+	private String nome;
+	private String local;
+	private String data;
+	private String horarioinicio;
+	
+	
+	public Evento() {
+		super();
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLocal() {
+		return local;
+	}
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getHorarioinicio() {
+		return horarioinicio;
+	}
+	public void setHorarioinicio(String horarioinicio) {
+		this.horarioinicio = horarioinicio;
+	}
+
+	public long getCodigo() {
+		return codigo;
+	}
+}
